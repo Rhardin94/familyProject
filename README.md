4df3f4c30731bf2d769d256b6b8c2ab6c21b2166
Robert Hardin <robertjameshardin@aol.com>
2019-12-23 12:27:14 -0600

    gathered almost all of the photos of family members, still need a few more and background images for each custom page.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # familyProject
+## familyProject is a scrapbook-esque collection of family stories and photos dedicated as a Christmas present because I'm just a poor boy.
---
+++ js/app.js
@@ -0,0 +1,49 @@
+const family = [
+    {
+        name: "Kelly",
+        picture: "assets/images/kelly/kelly1.jpeg"
+    },
+    {
+        name: "Gage",
+        picture: "assets/images/gage/gage1.jpeg"
+    },
+    {
+        name: "Dad",
+        picture: "assets/images/dad/dad2.jpg"
+    },
+    {
+        name: "Mom",
+        picture: "assets/images/mom/mom2.jpg"
+    },
+    {
+        name: "David",
+        picture: "assets/images/david/david1.jpg"
+    },
+    {
+        name: "Daniel",
+        picture: "assets/images/daniel/daniel1.jpg"
+    },
+    {
+        name: "Grammy",
+        picture: "assets/images/grammy/grammy1.jpg"
+    },
+    {
+        name: "Papa",
+        picture: "assets/images/papa/papa1.jpg"
+    }
+]
+$(document).ready(function () {
+    console.log(family)
+    for (let i = 0; i < family.length; i++) {
+        $(".familyCards").append(
+            $("<div>").addClass("col-lg-2 col-md-2 col-sm-2").append(
+                $("<div>").addClass("card m-2").append(
+                    $("<img>").addClass("card-img-top").attr("src", family[i].picture),
+                    $("<div>").addClass("card-body text-center").append(
+                        $("<h4>").text(family[i].name)
+                    )
+                )
+            )
+        )
+    }
+})
